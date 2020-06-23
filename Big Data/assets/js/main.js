5cdb020df4c72a2b05f79d48ddb043fad4142505
diraafwa <47936646+diraafwa@users.noreply.github.com>
2020-06-23 16:16:28 +0700

    Add files via upload

--- Big Data/assets/js/main.js
+++ Big Data/assets/js/main.js
@@ -72,43 +72,8 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
 
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
 
   // Clients carousel (uses the Owl Carousel library)
   $(".clients-carousel").owlCarousel({
